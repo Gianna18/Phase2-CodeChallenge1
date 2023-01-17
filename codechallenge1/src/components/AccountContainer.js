8188890441b655b43f02a02a00e1ec3e08c9fafe
Markmutuku <mark.mutuku@student.moringaschool.com>
2023-01-17 14:48:49 +0300

    Updated ReadMe

--- codechallenge1/src/components/AccountContainer.js
+++ codechallenge1/src/components/AccountContainer.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import TransactionsList from "./TransactionList";
-import Search from "./Search";
+import TransactionsList from "../TransactionList";
+import Search from "../Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
